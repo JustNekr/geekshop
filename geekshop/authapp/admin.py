8b6fcd4f1563225d7e5d11af4bee67d489b55662
JustNekr <justnekr@gmail.com>
2021-07-14 19:25:43 +0200

    change root_folder

--- geekshop/authapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ShopUser, ShopUserProfile
-from basketapp.models import Basket
-
-admin.site.register(ShopUser)
-admin.site.register(Basket)
-admin.site.register(ShopUserProfile)
