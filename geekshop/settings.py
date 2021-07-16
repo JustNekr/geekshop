6959376933abd3fd257bba1a4e3f4d86c1afcec1
JustNekr <justnekr@gmail.com>
2021-07-16 15:31:40 +0200

    - STATICFILES_DIRS

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -173,9 +173,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'geekshop', "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'geekshop', "static"),
+# )
 
 
 # Default primary key field type
