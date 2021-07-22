b70ae500fc2a9de6fd29c3dd1b633a1c8a9dc37c
JustNekr <justnekr@gmail.com>
2021-07-22 09:15:12 +0200

    testing some

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -4,8 +4,7 @@ from basketapp.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        users_basket = Basket(user=request.user)
-        basket_list = users_basket.get_items_cached
+        basket_list = Basket(user=request.user).get_items_cached
 #        basket_list = basket.get_items(request.user).select_related()
 #        basket_list = Basket.objects.filter(user=request.user).select_related()
     return {
