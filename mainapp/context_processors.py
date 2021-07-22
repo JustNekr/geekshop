811dad85c7120416eb5e4a1182b27708346b39dc
JustNekr <justnekr@gmail.com>
2021-07-22 08:30:47 +0200

    removed cached_property at get_items for context processor

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -4,8 +4,7 @@ from basketapp.models import Basket
 def basket(request):
     basket_list = []
     if request.user.is_authenticated:
-        basket_list = Basket.get_items_cached
-#        basket_list = Basket.get_items(request.user).select_related()
+        basket_list = Basket.get_items(request.user).select_related()
 #        basket_list = Basket.objects.filter(user=request.user).select_related()
     return {
         'basket': basket_list
