2d35775e0fbb1789d8f4e7c4a0514be6febf06dc
JustNekr <justnekr@gmail.com>
2021-07-22 09:00:20 +0200

    testing some

--- mainapp/context_processors.py
+++ mainapp/context_processors.py
@@ -7,5 +7,5 @@ def basket(request):
         basket_list = Basket.get_items(request.user).select_related()
 #        basket_list = Basket.objects.filter(user=request.user).select_related()
     return {
-        'basket': basket_list
+        'basket': basket_list, 'request': request
     }
