32adf5c69835915ecae3acf768d470cb55b1d826
JustNekr <justnekr@gmail.com>
2021-07-23 11:47:42 +0200

    cache_page removed from ProductsView

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,7 +6,7 @@ from .views import ProductsView, ProductView
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', cache_page(3600)(ProductsView.as_view()), name='products'),
+    path('', ProductsView.as_view(), name='products'),
     path('category/<int:pk>/', cache_page(3600)(ProductsView.as_view()), name='category'),
     # path('category/<int:pk>/page/<int:page>', ProductsView.as_view(), name='page'),
     path('product/<int:pk>/', cache_page(3600)(ProductView.as_view()), name='product'),
