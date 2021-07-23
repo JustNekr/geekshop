964ea2a1666ffb0e3d09703925457fa5ae06a2ee
JustNekr <justnekr@gmail.com>
2021-07-23 11:40:24 +0200

    cache_page at ProductsView and category

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,8 +6,8 @@ from .views import ProductsView, ProductView
 app_name = 'mainapp'
 
 urlpatterns = [
-    path('', ProductsView.as_view(), name='products'),
-    path('category/<int:pk>/', ProductsView.as_view(), name='category'),
+    path('', cache_page(3600)(ProductsView.as_view()), name='products'),
+    path('category/<int:pk>/', cache_page(3600)(ProductsView.as_view()), name='category'),
     # path('category/<int:pk>/page/<int:page>', ProductsView.as_view(), name='page'),
     path('product/<int:pk>/', cache_page(3600)(ProductView.as_view()), name='product'),
 ]
