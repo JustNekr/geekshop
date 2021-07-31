0f7841ee33addc545789a0cf3c3c8d605a52eb0b
JustNekr <justnekr@gmail.com>
2021-07-31 18:24:32 +0200

    updated test at authapp

--- authapp/tests.py
+++ authapp/tests.py
@@ -12,6 +12,8 @@ class TestUserManagement(TestCase):
     super_username = 'django2'
     super_email = 'django2@geekshop.local'
     super_password = 'geekbrains'
+    status_code_success = 200
+    status_code_redirect = 302
 
     def setUp(self):
         # call_command('flush', '--noinput')
@@ -23,23 +25,26 @@ class TestUserManagement(TestCase):
     def test_user_login(self):
         # главная без логина
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertTrue(response.context['user'].is_anonymous)
         self.assertEqual(response.context['title'], 'Главная')
-        self.assertNotContains(response, 'Пользователь', status_code=200)
+        self.assertNotContains(response, 'Пользователь', status_code=self.status_code_success)
         # self.assertNotIn('Пользователь', response.content.decode())
 
         # данные пользователя
+        # логинимся
         self.client.login(username=self.username, password=self.password)
 
-        # логинимся
+        response = self.client.get('auth/login/')
+        self.assertEqual(response.status_code, self.status_code_redirect)
+
         response = self.client.get('/')
         self.assertFalse(response.context['user'].is_anonymous)
         self.assertEqual(response.context['user'], self.user)
 
         # главная после логина
         response = self.client.get('/')
-        self.assertContains(response, 'Пользователь', status_code=200)
+        self.assertContains(response, 'Пользователь', status_code=self.status_code_success)
         self.assertEqual(response.context['user'], self.user)
         # self.assertIn('Пользователь', response.content.decode())
 
@@ -47,13 +52,13 @@ class TestUserManagement(TestCase):
         # без логина должен переадресовать
         response = self.client.get('/basket/')
         self.assertEqual(response.url, '/auth/login/?next=/basket/')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, self.status_code_redirect)
 
         # с логином все должно быть хорошо
         self.client.login(username=self.username, password=self.password)
 
         response = self.client.get('/basket/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertEqual(list(response.context['basket']), [])
         self.assertEqual(response.request['PATH_INFO'], '/basket/')
         # self.assertIn('Ваша корзина, Пользователь', response.content.decode())
@@ -64,22 +69,22 @@ class TestUserManagement(TestCase):
 
         # логинимся
         response = self.client.get('/auth/login/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertFalse(response.context['user'].is_anonymous)
 
         # выходим из системы
         response = self.client.get('/auth/logout/')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, self.status_code_redirect)
 
         # главная после выхода
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertTrue(response.context['user'].is_anonymous)
 
     def test_user_register(self):
         # логин без данных пользователя
         response = self.client.get('/auth/register/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertEqual(response.context['title'], 'регистрация')
         self.assertTrue(response.context['user'].is_anonymous)
 
@@ -93,26 +98,26 @@ class TestUserManagement(TestCase):
             'age': '21'}
 
         response = self.client.post('/auth/register/', data=new_user_data)
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, self.status_code_redirect)
 
         new_user = ShopUser.objects.get(username=new_user_data['username'])
 
         activation_url = f"{settings.DOMAIN_NAME}/auth/verify/{new_user_data['email']}/{new_user.activation_key}/"
 
         response = self.client.get(activation_url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
 
         # данные нового пользователя
         self.client.login(username=new_user_data['username'], password=new_user_data['password1'])
 
         # логинимся
         response = self.client.get('/auth/login/')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertFalse(response.context['user'].is_anonymous)
 
         # проверяем главную страницу
         response = self.client.get('/')
-        self.assertContains(response, text=new_user_data['first_name'], status_code=200)
+        self.assertContains(response, text=new_user_data['first_name'], status_code=self.status_code_success)
 
     def test_user_wrong_register(self):
         new_user_data = {
@@ -125,6 +130,6 @@ class TestUserManagement(TestCase):
             'age': '17'}
 
         response = self.client.post('/auth/register/', data=new_user_data)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, self.status_code_success)
         self.assertFormError(response, 'register_form', 'age', 'Вы слишком молоды!')
 
